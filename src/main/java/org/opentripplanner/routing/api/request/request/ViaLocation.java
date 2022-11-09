a7d5d7dc51cac9ad57bbd5eedc592916c4a2c16b
Hannes Junnila <hannes.junnila@gmail.com>
2022-11-10 01:11:12 +0200

    WIP: VIA routing

--- src/main/java/org/opentripplanner/routing/api/request/request/ViaLocation.java
+++ src/main/java/org/opentripplanner/routing/api/request/request/ViaLocation.java
@@ -1,26 +1,36 @@
 package org.opentripplanner.routing.api.request.request;
 
 import java.time.Duration;
+import java.util.Objects;
 import org.opentripplanner.model.GenericLocation;
 
-// TODO VIA: JavaDoc
-public class ViaLocation {
+/**
+ * Represents a point to pass on trip.
+ *
+ * @param point            Coordinates of Via point
+ * @param passThroughPoint Does the via point represent a pass through
+ * @param minSlack         Minimum time that is allowed to wait for interchange.
+ * @param maxSlack         Maximum time to wait for next departure.
+ */
+public record ViaLocation(
+  GenericLocation point,
+  boolean passThroughPoint,
+  Duration minSlack,
+  Duration maxSlack
+) {
+  public static final Duration DEFAULT_MAX_SLACK = Duration.ofMinutes(60L);
+  public static final Duration DEFAULT_MIN_SLACK = Duration.ofMinutes(0L);
 
-  private GenericLocation point;
-  private boolean passThroughPoint = false;
-
-  // TODO VIA Part 2: Is this minimum, should it be named so?
-  private Duration slack = Duration.ofMinutes(60);
-
-  public GenericLocation point() {
-    return point;
+  public ViaLocation {
+    Objects.requireNonNull(minSlack);
+    Objects.requireNonNull(maxSlack);
   }
 
-  public boolean passThroughPoint() {
-    return passThroughPoint;
+  public ViaLocation(GenericLocation point, boolean passThroughPoint, Duration minSlack) {
+    this(point, passThroughPoint, minSlack, DEFAULT_MAX_SLACK);
   }
 
-  public Duration slack() {
-    return slack;
+  public ViaLocation(GenericLocation point, boolean passThroughPoint) {
+    this(point, passThroughPoint, DEFAULT_MIN_SLACK, DEFAULT_MAX_SLACK);
   }
 }

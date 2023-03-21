c6861984636b65ef86cd083f9c5b2742a043794f
Thomas Gran <t2gran@gmail.com>
2023-03-21 14:00:51 +0100

    Add "relaxTransitPriorityGroup" to REST API
    This enables us to pass in the relax function to use with
    transit-group-priority (model not implemented jet).

--- src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/mappers/RaptorRequestMapper.java
+++ src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/transit/mappers/RaptorRequestMapper.java
@@ -8,14 +8,19 @@ import java.time.ZonedDateTime;
 import java.util.Collection;
 import org.opentripplanner.framework.application.OTPFeature;
 import org.opentripplanner.raptor.api.RaptorConstants;
+import org.opentripplanner.raptor.api.model.GeneralizedCostRelaxFunction;
 import org.opentripplanner.raptor.api.model.RaptorAccessEgress;
+import org.opentripplanner.raptor.api.model.RelaxFunction;
 import org.opentripplanner.raptor.api.request.Optimization;
 import org.opentripplanner.raptor.api.request.RaptorRequest;
 import org.opentripplanner.raptor.api.request.RaptorRequestBuilder;
 import org.opentripplanner.raptor.rangeraptor.SystemErrDebugLogger;
 import org.opentripplanner.routing.algorithm.raptoradapter.router.performance.PerformanceTimersForRaptor;
 import org.opentripplanner.routing.algorithm.raptoradapter.transit.TripSchedule;
+import org.opentripplanner.routing.algorithm.raptoradapter.transit.cost.RaptorCostConverter;
+import org.opentripplanner.routing.algorithm.raptoradapter.transit.cost.grouppriority.TransitPriorityGroup32n;
 import org.opentripplanner.routing.api.request.RouteRequest;
+import org.opentripplanner.routing.api.request.preference.Relax;
 
 public class RaptorRequestMapper {
 
@@ -100,11 +105,13 @@ public class RaptorRequestMapper {
       searchParams.numberOfAdditionalTransfers(preferences.transfer().maxAdditionalTransfers());
     }
     builder.withMultiCriteria(mcBuilder -> {
-      preferences
-        .transit()
-        .raptor()
-        .relaxGeneralizedCostAtDestination()
-        .ifPresent(mcBuilder::withRelaxCostAtDestination);
+      var r = preferences.transit().raptor();
+      if (!r.relaxTransitPriorityGroup().isNormal()) {
+        mcBuilder.withTransitPriorityCalculator(TransitPriorityGroup32n.priorityCalculator());
+        mcBuilder.withRelaxC1(mapRelaxCost(r.relaxTransitPriorityGroup()));
+      } else {
+        r.relaxGeneralizedCostAtDestination().ifPresent(mcBuilder::withRelaxCostAtDestination);
+      }
     });
 
     for (Optimization optimization : preferences.transit().raptor().optimizations()) {
@@ -157,6 +164,16 @@ public class RaptorRequestMapper {
     );
 
     return builder.build();
+  }
+
+  static RelaxFunction mapRelaxCost(Relax relax) {
+    if (relax == null) {
+      return null;
+    }
+    return GeneralizedCostRelaxFunction.of(
+      relax.ratio(),
+      RaptorCostConverter.toRaptorCost(relax.slack())
+    );
   }
 
   private int relativeTime(Instant time) {

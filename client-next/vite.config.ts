8eaa62328fa315ca4ec552bffffb895087a4d642
Tom Erik Støwer <testower@gmail.com>
2023-12-15 11:45:26 +0100

    Example how to use versions in generated files names

--- client-next/vite.config.ts
+++ client-next/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const VERSION = 'v1';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +10,18 @@ export default defineConfig({
   build: {
     outDir: '../src/client/debug-client-preview',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: () => {
+          return `[name].${VERSION}.css`;
+        },
+        chunkFileNames: () => {
+          return `[name].${VERSION}.js`;
+        },
+        entryFileNames: () => {
+          return `[name].${VERSION}.js`;
+        },
+      },
+    },
   },
 });
